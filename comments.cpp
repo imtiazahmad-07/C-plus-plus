ab82f98acb6b3ee3f70d54fd28135df433d97b79
Imtiaz Baloch <137213444+Imtiazbaloch00@users.noreply.github.com>
2023-12-12 16:56:44 +0500

    Add files via upload

---
+++ comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	//Single line
+	
+	std::cout<<"Print this";
+	// The above/below lines are comments which means it is an additional information which does not affect the code
+	
+	/*This is a 
+	multi line comment*/
+	
+	
+}
