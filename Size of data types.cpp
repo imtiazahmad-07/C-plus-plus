38ed033e91b23d7d4c6f5d755b9b4f2ca8f0c9ad
Imtiaz Baloch <137213444+Imtiazbaloch00@users.noreply.github.com>
2023-12-13 18:53:28 +0000

    Add files via upload

---
+++ Size of data types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int var1;
+	float var2;
+	double var3;
+	string var4;
+	char var5;
+	bool var6;
+	cout<<"Size of integer is "<<sizeof(var1)<<endl;
+	cout<<"Size of float is "<<sizeof(var2)<<endl;
+	cout<<"Size of double is "<<sizeof(var3)<<endl;
+	cout<<"Size of string is "<<sizeof(var4)<<endl;
+	cout<<"Size of charachter is "<<sizeof(var5)<<endl;
+	cout<<"Size of boolean is "<<sizeof(var6)<<endl;
+}
