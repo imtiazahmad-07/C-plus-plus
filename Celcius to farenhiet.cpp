48b5b89a04753baa7b96cca3132ffadb108a648b
Imtiaz Baloch <137213444+Imtiazbaloch00@users.noreply.github.com>
2023-12-13 18:40:01 +0000

    Add files via upload

---
+++ Celcius to farenhiet.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float Celcius;
+    float Farenhiet;
+    
+    cout<<"input temperature in Celsius: "<<endl;
+    cin>>Celcius;
+
+    Farenhiet = (Celcius * 9 / 5) + 32;
+
+    cout<<Celcius<<" Celsius = "<<Farenhiet<<" Fahrenheit"<<endl;
+
+    return 0;
+}
