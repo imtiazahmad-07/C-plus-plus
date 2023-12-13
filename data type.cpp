f5bf6c46bcada739547b6e9cedee40eb6c7fdf9b
Imtiaz Baloch <137213444+Imtiazbaloch00@users.noreply.github.com>
2023-12-13 15:13:08 +0000

    Add files via upload

---
+++ data type.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//Data types in Cpp
+	//1. Integer
+	int age=20;
+	cout<<"Your age is "<<age<<endl;
+	//2. FLoating points 
+	float pi=3.14;
+	cout<<"Value of pi is "<<pi<<endl;
+	//3. string
+	string name="Imtiaz";
+	cout<<"Hey! THis is "<<name<<endl;
+	//4. character
+	char fav_letter='M';
+	cout<<"My favourite letter is "<<fav_letter<<endl;
+	//5. Boolean
+	bool right=true;
+	cout<<"You are on position "<<right<<endl;
+	//6. Double. this stores 15 decimal numbers
+	double decimal_number=3.4523453;
+	cout<<"Decimal number is : "<<decimal_number<<endl;
+	return 0;
+	
+}
