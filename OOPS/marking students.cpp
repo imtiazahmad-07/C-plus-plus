ea73824e857ef3f2227f20e659c1d5c01d70cfab
Imtiaz Ahmad <137213444+imtiazahmad-07@users.noreply.github.com>
2024-05-12 18:16:59 +0500

    Using comments in marking students.cpp file

--- OOPS/marking students.cpp
+++ OOPS/marking students.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    int array[5]={10,20,30,40,50};
-    string names[5]={"imtiaz","waseem","haroon","kabeer","sohail"};
-    for(int i=0; i<5; i++)
+    int array[5]={10,20,30,40,50};                                        //Initializing the marks of Students   
+    string names[5]={"imtiaz","waseem","haroon","kabeer","sohail"};       //intializing the names of students
+    for(int i=0; i<5; i++)                                                //using a loop to print the arrays
     {
-        cout<<names[i]<<" got "<<array[i]<<" marks"<<endl;
+        cout<<names[i]<<" got "<<array[i]<<" marks"<<endl;                // printing the both arrays to show the names and marks of the students
     }
 } 
