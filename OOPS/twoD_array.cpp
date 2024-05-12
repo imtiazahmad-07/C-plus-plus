d3c5b95b2296440b8992e083c8653f9fbfb6e560
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-12 18:34:34 +0500

    Modified and wrote the code of 2d array

--- OOPS/twoD_array.cpp
+++ OOPS/twoD_array.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+        int matrix[2][2];
+        cout<<"Enter the elements of matrix :"<<endl;
+        for(int i=0; i<2; i++)
+        {
+            for(int j=0; j<2; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        cout<<"You entered the elements, below is the 2x2 matrix"<<endl;
+        for(int i=0; i<2; i++)
+        {
+            for(int j=0; j<2; j++)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
 }
