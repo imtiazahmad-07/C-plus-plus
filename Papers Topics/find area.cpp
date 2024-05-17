385ccad0b80fe350b68ca52f9c2df7aa2b32c70d
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-17 16:04:43 +0500

    created a constructor to find area

--- Papers Topics/find area.cpp
+++ Papers Topics/find area.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+class find_area{
+    private:
+        int length,width,area;
+    public:
+        find_area()
+        {
+            cout<<"Enter length in meters : ";
+            cin>>length;
+            cout<<"Enter width in meters : ";
+            cin>>width;
+            area = length * width;
+            cout<<"Area is "<<area<<" meters";
+        }
+};
 int main()
 {
     
