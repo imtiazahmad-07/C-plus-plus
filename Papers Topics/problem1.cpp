8ec7d98eb3084bb22f91ff0a0499cb2feb81e771
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-17 12:14:14 +0500

    Had some comments to clarify the program

--- Papers Topics/problem1.cpp
+++ Papers Topics/problem1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-class person{
-    public:
-    int age;
+class person{           //declared a class with a name "Person"
+    public:             //access specifier for data members (means for the variables)
+    int age;             
     string name;
     string address;
-    public:
-    void input()
+    public:             //acess specifier for method funtions
+    void input()        //declared and defined the method function for getting input from user
     {
         cout<<"Enter your name : ";
         cin>>name;
@@ -15,23 +15,23 @@ class person{
         cout<<"Enter your address : ";
         cin>>address;
     }
-    void output()
+    void output()       //declared and defined the method function for giving user's data as an output 
     {
         cout<<"\t Name : "<<name<<ends;
         cout<<"\t Age : "<<age<<ends;
-        cout<<"\t Address : "<<address<<ends;
+        cout<<"\t Address : "<<address<<ends;       //here ends is used to have an space 
         cout<<endl;
     }
 
 };
 int main()
 {
-    person person1,person2,person3;
-    person1.input();
-    person1.output();
-    person2.input();
-    person2.output();
-    person3.input();
-    person3.output();
+    person person1,person2,person3; //using class "person" to create three objects person1,2,3
+    person1.input();                //person1's input data
+    person1.output();               //person1's output data
+    person2.input();                //person2's input data
+    person2.output();               //person2's output data
+    person3.input();                //person3's input data
+    person3.output();               //person3's output data
 
 }
