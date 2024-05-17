f644e33c5543c34d7ac509b181652a49d8481bd5
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-17 13:11:58 +0500

    wrote the code and modified it using comments in it

--- Papers Topics/local_class.cpp
+++ Papers Topics/local_class.cpp
@@ -1,12 +1,33 @@
+
+
+//////////////////////////////========== Summing up two numbers using a local class =============////////////////////////////////////
+
+
 #include <iostream>
 using namespace std;
-void function()
+void function()         //declared and defined a function
 {
-    class sum{
-        
+    class sum{          //declared and defined a class
+        private:        //used access specifier for data members
+        int a,b;        //declared to values to sum up 
+        public:         //access specifier for method function
+        void input()    //defined a function to take input
+        {
+            cout<<"Enter first number : ";
+            cin>>a;
+            cout<<"Enter second number : ";
+            cin>>b;
+        }
+        void display()  //defined a function to display the given input and sum of the numbers
+        {
+            cout<<a<<" + "<<b<<" = "<<endl;
+        }
     };
+    sum obj;    //declared an object for class "sum"
+    obj.input();//called the mehtod function "input" by using a dot operator
+    obj.display();//called the member/method function "display" through an object using dot operator
 }
 int main()
 {
-      
+        function();   //called the function to display everything within the function including the local class      
 } 
