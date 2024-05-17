d20c21b672ea94ab000206d7ea83eadfe7222c19
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-17 17:22:23 +0500

    created the file and wrote the code

---
+++ Papers Topics/destructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int count=0;
+class check{
+    
+    public:
+    check()  //declaring the constructor
+    {
+        cout<<"Obj "<<++count<<" allocated space"<<endl;
+    }
+    ~check() //declaring the destructor which deallocated the space in memory
+    {
+        cout<<"Obj "<<count--<<" deallocated the space"<<endl;
+    }
+};
+int main()
+{
+    check obj1,obj2; //creating objects to check the destructor which works in the reverse order
+
+}
