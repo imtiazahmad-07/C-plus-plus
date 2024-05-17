aad46c4d69f73dac0945600a62b362682bbe034a
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-17 12:04:09 +0500

    Modifiied the code

--- Papers Topics/problem1.cpp
+++ Papers Topics/problem1.cpp
@@ -5,9 +5,32 @@ class person{
     int age;
     string name;
     string address;
+    public:
+    void input()
+    {
+        cout<<"Enter your name : ";
+        cin>>name;
+        cout<<"Enter your age : ";
+        cin>>age;
+        cout<<"Enter your address : ";
+        cin>>address;
+    }
+    void output()
+    {
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+        cout<<"Address : "<<address<<endl;
+    }
 
 };
 int main()
 {
+    person imtiaz,waseem,haroon;
+    imtiaz.input();
+    imtiaz.output();
+    waseem.input();
+    waseem.output();
+    haroon.input();
+    haroon.output();
 
 }
