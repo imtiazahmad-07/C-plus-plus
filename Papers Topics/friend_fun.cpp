fb4eea123db9d6ead7a34d41ba80a3d7ec5a229c
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-18 12:19:55 +0500

    defined the friend function outside the class

--- Papers Topics/friend_fun.cpp
+++ Papers Topics/friend_fun.cpp
@@ -13,6 +13,13 @@ private:                        //access specifier for data members
     }
      friend int add_numbers(calculator, int a, int b);  
 };
+int add_numbers(calculator obj, int a, int b)   //defining the friend function and here we ceate an object of the above class to access the private data members
+{
+    obj.first = a; //storing the value of a in private data member "first" by using an object
+    obj.second= b;
+    ////storing the value of b in private data member "second" by using an object
+    return obj.add(); //calling the member function to add the both numbers
+}
 int main()
 {
     
