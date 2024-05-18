e35b20fead7d8e1d7f23595f734128c12efc03fb
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-18 12:19:01 +0500

    created a class

--- Papers Topics/friend_fun.cpp
+++ Papers Topics/friend_fun.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
+class calculator                //declaring a class
+{
+private:                        //access specifier for data members
+    int first, second;
+
+
+    public:             //access specifier for member function 
+    int add()           //declaring a member function
+    {
+        return first + second;      //adding to numbers and returning these to the member function
+    }
+};
 int main()
 {
     
