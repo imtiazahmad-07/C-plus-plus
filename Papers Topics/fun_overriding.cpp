a9ce03be89c5651730f85d87c9a3de00237771b0
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-22 20:27:14 +0500

    had some comments

--- Papers Topics/fun_overriding.cpp
+++ Papers Topics/fun_overriding.cpp
@@ -3,14 +3,14 @@ using namespace std;
 class class1
 {
 public:
-    void message()
+    void message()          //created a public member function
     {
         cout<<"Hi"<<endl;
     }
 };
-class class2: public class1{
+class class2: public class1{    //inheriting the  properties of class 1 to class 2
     public:
-    void message(){
+    void message(){             //declaring a function 
         cout<<"Hello"<<endl;
     }
 };
@@ -19,8 +19,8 @@ class class2: public class1{
 
 int main()
 {
-    class2 obj;
-    obj.class1::message();
-    obj.message();
+    class2 obj;         //declaring an object
+    obj.class1::message();  //the function of class1 will be called 
+    obj.message();          //the function of class2 will be called
 
 }
