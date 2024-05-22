2c32ce060f9a75e6e644e05135cf2b4ec4a53376
Imtiaz Ahmad <7807imtiaz@gmail.com>
2024-05-22 20:56:10 +0500

    had some comments

--- Papers Topics/inheritence.cpp
+++ Papers Topics/inheritence.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-class numbers{
-    protected:
-        int num1,num2;
-        public:
-        void input()
+class numbers{          //declaring a class
+    protected:          //access specifier for data members
+        int num1,num2;      
+        public:         //access specifier for member function
+        void input()    //creating a member function
         {
             cout<<"Enter first number ";
             cin>>num1;
@@ -12,18 +12,18 @@ class numbers{
             cin>>num2;
         }
 };
-class product: public numbers{
-    int product;
+class product: public numbers{          //structure to inherit the properties of class "numbers"
+    int product;            
     public:
     void multiply()
     {
-        product=num1*num2;
+        product=num1*num2;              //accessing the protected data members due to inheritence 
         cout<<"product is : "<<product;
     }
 };
 int main()
 {
     product obj;
-    obj.input();
-    obj.multiply();
+    obj.input();            //calling input member function
+    obj.multiply();         //calling multiply function of which inherited the above class' member function
 }
