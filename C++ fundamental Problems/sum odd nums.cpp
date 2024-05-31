8da39c6e8bac7444029425e9e83529c4be098d7e
Imtiaz Ahmad <137213444+imtiazahmad-07@users.noreply.github.com>
2024-05-31 19:55:18 +0500

    Update sum odd nums.cpp

--- C++ fundamental Problems/sum odd nums.cpp
+++ C++ fundamental Problems/sum odd nums.cpp
@@ -2,10 +2,10 @@
 using namesapcae std; 
 int main()
 {
-  int sum=0;
-  for(int i=1; i<=15; i++)
+  int sum=0;                         //initialize sum with zero value
+  for(int i=1; i<=15; i++)          //given that : sum up the numbers form 1 to 15 and for that we uses for loop 
     {
-      sum=sum+i;  
+      sum=sum+i;                    //with each iteration the variable "sum" adds its own and the variable of loop
     }
-  cout<<"Sum is : "<<sum<<endl;
+  cout<<"Sum is : "<<sum<<endl;     //printing the values which are added from 1 to 5 whose output becomes "Sum is : 64"
 }
